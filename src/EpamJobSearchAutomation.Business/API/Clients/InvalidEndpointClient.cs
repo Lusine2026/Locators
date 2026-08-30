2a555537ef907d34e52e21046cfc4ce74c8d038d
Lusine Hovhannisyan <lusine_hovhannisyan@epam.com>
2026-08-31 02:23:27 +0400

    API Home task

---
+++ src/EpamJobSearchAutomation.Business/API/Clients/InvalidEndpointClient.cs
@@ -0,0 +1,24 @@
+﻿using EpamJobSearchAutomation.Framework.API;
+using EpamJobSearchAutomation.Framework.API.Builders;
+using EpamJobSearchAutomation.Framework.Logging;
+using RestSharp;
+
+namespace EpamJobSearchAutomation.Business.API.Clients;
+
+public class InvalidEndpointClient : ApiClient
+{
+    public async Task<RestResponse> GetInvalidEndpointAsync()
+    {
+        Logger.Info("Sending GET request to /invalidendpoint");
+
+        var request = new ApiRequestBuilder("invalidendpoint", Method.Get)
+            .Build();
+
+        var response = await Client.ExecuteAsync(request);
+
+        Logger.Info($"Received response with status code: {(int)response.StatusCode}");
+
+        return response;
+    }
+}
+
---
+++ tests/EpamJobSearchAutomation.API.Tests/Tests/ApiTestBase.cs
@@ -0,0 +1,24 @@
+﻿using EpamJobSearchAutomation.Framework.Logging;
+
+namespace EpamJobSearchAutomation.API.Tests.Tests;
+
+public abstract class ApiTestBase
+{
+    [SetUp]
+    public void Setup()
+    {
+        Logger.Configure();
+
+        Logger.Info($"Starting API test: {TestContext.CurrentContext.Test.Name}");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        var status = TestContext.CurrentContext.Result.Outcome.Status;
+
+        Logger.Info($"API test finished: {TestContext.CurrentContext.Test.Name}");
+        Logger.Info($"Test result status: {status}");
+    }
+}
+
